ccedb9048f71d078ce079b80224042e8b6600833
Neop <neop@gmail.com>
2023-09-12 09:20:19 +1200

    docker install and portainer

--- customscript.sh
+++ customscript.sh
@@ -4,6 +4,6 @@ apt -y install apache2
 apt -y install cockpit
 apt -y install docker.io
 echo "Hello world from $(hostname) $(hostname -I)" > /var/www/html/index.html
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 apt -y upgrade
