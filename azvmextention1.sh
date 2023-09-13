a7c0554ca565fd920471cd511a00ff599d85a75a
Neop <neop@gmail.com>
2023-09-13 14:38:23 +1200

    commented apache

--- azvmextention1.sh
+++ azvmextention1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-apt update 
-apt -y install apache2
-apt -y install cockpit
-apt -y install docker.io
-echo "Hello world from $(hostname) $(hostname -I)" > /var/www/html/index.html
+sudo apt update
+sudo apt install wget curl nano software-properties-common dirmngr apt-transport-https gnupg gnupg2 ca-certificates lsb-release ubuntu-keyring unzip -y 
+#sudo apt -y install apache2
+sudo apt -y install cockpit
+sudo apt -y install docker.io
+#echo "Hello world from $(hostname) $(hostname -I)" > /var/www/html/index.html
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 apt -y upgrade
